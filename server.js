9c90b5d98bd8d821913f3a27ace51edcbef32437
totxprex <totxprex@gmail.com>
2022-11-18 12:49:20 +0100

    b

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const dotenv = require("dotenv")
-dotenv.config({ path: "./config.env" })
+// dotenv.config({ path: "./config.env" })
 const express = require("express")
 const mongoose = require("mongoose")
 const morgan = require("morgan")
